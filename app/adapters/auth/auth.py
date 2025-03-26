92d453b584b41112e058ccbe8693215677834529
LUCAS LOPES <lucaslopesfnance@gmail.com>
2025-03-26 17:25:03 -0300

    Update
    Ajustes

--- app/adapters/auth/auth.py
+++ app/adapters/auth/auth.py
@@ -1,24 +1,6 @@
-from flask_jwt_extended import JWTManager, create_access_token
-from flask import Flask, request, jsonify
-from app.core.models import db, Seller
-from werkzeug.security import check_password_hash
+from flask_jwt_extended import JWTManager
 
-app = Flask(__name__)
-app.config.from_object('config.Config')
-db.init_app(app)
+jwt = JWTManager()
 
-jwt = JWTManager(app)
-
-@app.route('/api/auth/login', methods=['POST'])
-def login():
-    data = request.get_json()
-    seller = Seller.query.filter_by(email=data['email']).first()
-
-    if seller and check_password_hash(seller.senha, data['senha']):
-        if seller.status == 'Inativo':
-            return jsonify({'message': 'Conta inativa. Ative sua conta primeiro.'}), 401
-
-        access_token = create_access_token(identity=seller.id)
-        return jsonify({'access_token': access_token})
-
-    return jsonify({'message': 'Credenciais inválidas!'}), 401
+def configurar_auth(app):
+    jwt.init_app(app)
