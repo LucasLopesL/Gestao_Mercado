343e1742eabf49fbbbb0a08dfb931239e5b92cd3
viniciusdrpaula <viniciusdrpaula@gmail.com>
2025-03-31 17:28:56 -0300

    Update twillio_service.py

--- app/adapters/twillio/twillio_service.py
+++ app/adapters/twillio/twillio_service.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
 
 def enviar_codigo_ativacao(celular, codigo):
-    account_sid = ''
-    auth_token = ''
+    account_sid = 'ACfbef0f945503dc98894a6c205f1e8560'
+    auth_token = '8299a56fc293fabeec3cacc603c2dd31'
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
         body=f"Seu código de ativação é {codigo}",
-        from_='',  # Número do Twilio
+        from_='+14155238886',  # Número do Twilio
         to=celular
     )
     return message.sid
